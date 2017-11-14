1b7ac520355aa5abb531d3020812c17eb3e8fae6
DenverBYF <851166182@qq.com>
2017-11-14 23:42:10 +0800

    添加工作日称模块,完善中

--- resources/views/admin/work/index.blade.php
+++ resources/views/admin/work/index.blade.php
@@ -12,23 +12,7 @@
 
 @section('content')
     <div class="container">
-        <div class="col-md-3 col-sm-3">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion"
-                                href="#collapseTwo">
-                            点击我进行展开，再次点击我进行折叠。第 2 部分
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseTwo" class="panel-collapse collapse">
-                    <div class="panel-body">
 
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
 
