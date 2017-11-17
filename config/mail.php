a3992aa856077ee127a61c80054f4d281bc0752f
DenverBYF <851166182@qq.com>
2017-11-17 10:32:35 +0800

    修改语言

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', '18991388969@163.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('group_name', 'Example'),
     ],
 
     /*
